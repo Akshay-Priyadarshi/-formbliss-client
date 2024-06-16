d23e159f8ad2dc2c60afbf8417b1775b2dfdaad3
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-06-16 18:09:59 +0530

    XX

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import type { Component } from "solid-js"
 import DynamicForm from "./components/DynamicForm"
 import registrationForm from "./forms/registration.json"
-import loginForm from "./forms/login.json"
 
 const App: Component = () => {
     return (
         <div class="flex items-center justify-center">
             <DynamicForm form={registrationForm} />
-            <DynamicForm form={loginForm} />
         </div>
     )
 }
