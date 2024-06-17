3e1cd97424fb94272adbd3a11c3db1816cf7f2f3
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-06-17 23:52:34 +0530

    Made logo a component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { A } from "@solidjs/router"
+import { Component } from "solid-js"
+
+const Logo: Component = props => {
+    return (
+        <A href="/" class="flex items-center">
+            <img
+                src="src/assets/android-chrome-192x192.png"
+                alt="Logo"
+                class="w-10 h-10"
+            />
+            <div class="w-4"></div>
+            <h1 class="text-4xl">FormBliss</h1>
+        </A>
+    )
+}
+
+export default Logo
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,11 @@
 import { A } from "@solidjs/router"
 import { Component } from "solid-js"
+import Logo from "./Logo"
 
 const NavBar: Component<{}> = () => {
     return (
-        <div class="flex justify-between items-center">
-            <A href="/" class="flex items-center">
-                <img
-                    src="src/assets/android-chrome-512x512.png"
-                    alt="Logo"
-                    class="w-10 h-10"
-                />
-                <div class="w-4"></div>
-                <h1 class="text-4xl">FormBliss</h1>
-            </A>
+        <div class="flex justify-between items-center py-[2rem] px-[4rem]">
+            <Logo />
             <ul class="flex">
                 <li class="text-xl">
                     <A href="/dashboard">
