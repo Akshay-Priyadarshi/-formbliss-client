da86979aa88723d0e715289d1f923f0ae2dcefc5
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-06-18 13:20:46 +0530

    Moved everything to modules

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const taildwindcssConfig = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {}
     },
     plugins: []
 }
+export default taildwindcssConfig
